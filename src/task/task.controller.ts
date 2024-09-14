dd380aeb33a154997ead713324cd744dd3aac5bb
abidmul <mulawaabid@gmail.com>
2024-09-14 20:01:03 +0700

    tahap-11-develop_role_permission_2_terbaru

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -19,7 +19,7 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { GetUser } from 'src/common/decorators/user.decorator';
 import { PoliciesGuard } from 'src/policies/policies.guard';
 import { Policies } from 'src/common/decorators/policies.decorator';
-import { EditTasks, DeleteTasks } from 'src/policies/task.policies'; // Import policies
+import { EditTasks, DeleteTasks } from 'src/policies/task.policies';
 
 const prisma = new PrismaClient();
 
@@ -62,9 +62,8 @@ export class TaskController {
     };
   }
 
-  // Policy untuk mengedit task
   @Get(':id/edit')
-  @Policies(new EditTasks()) // Cek permission untuk edit
+  @Policies(new EditTasks())
   @Render('task/edit')
   async edit(
     @Param('id') id: number,
@@ -84,9 +83,8 @@ export class TaskController {
     };
   }
 
-  // Policy untuk menghapus task
   @Get(':id/delete')
-  @Policies(new DeleteTasks()) // Cek permission untuk delete
+  @Policies(new DeleteTasks())
   @Render('task/delete')
   async delete(
     @Param('id') id: number,
@@ -104,7 +102,7 @@ export class TaskController {
   }
 
   @Delete(':id/destroy')
-  @Policies(new DeleteTasks()) // Cek permission untuk delete
+  @Policies(new DeleteTasks())
   @Redirect('/task')
   async destroy(@Param('id') id: string) {
     await prisma.task.delete({
@@ -129,7 +127,7 @@ export class TaskController {
   }
 
   @Put(':id/update')
-  @Policies(new EditTasks()) // Cek permission untuk update
+  @Policies(new EditTasks())
   @Redirect('/task')
   async update(@Param('id') id: number, @Body() task: Task) {
     const data = {
@@ -155,7 +153,7 @@ export class TaskController {
     let tasks: TaskModel[];
 
     const userPermissions = req['userPermissions'];
-    const user = req['user']; // Ambil user dari request
+    const user = req['user'];
 
     if (userPermissions.includes('view-any-tasks')) {
       tasks = await prisma.task.findMany({
@@ -191,14 +189,38 @@ export class TaskController {
 
   @Patch('move/:id')
   @Redirect('/task/progress')
-  async move(@Param('id') id: string, @Query('status') status: Status) {
+  async move(
+    @Param('id') id: string,
+    @Query('status') status: Status,
+    @Req() req: Request,
+  ): Promise<void> {
+    const userPermissions = req['userPermissions'];
+    const user = req['user'];
+
+    let task: TaskModel;
+
+    if (userPermissions.includes('edit-any-tasks')) {
+      task = await prisma.task.findUnique({
+        where: { id: Number(id) },
+      });
+    } else {
+      task = await prisma.task.findFirst({
+        where: {
+          id: Number(id),
+          userId: user.id,
+        },
+      });
+
+      if (!task) {
+        throw new Error(
+          'Task not found or you do not have permission to move this task.',
+        );
+      }
+    }
+
     await prisma.task.update({
-      where: {
-        id: Number(id),
-      },
-      data: {
-        status,
-      },
+      where: { id: Number(id) },
+      data: { status },
     });
   }
 }
